1a52e729e47bcd93e4390b5743359aa937b8f450
Lee Changseok <leechangseok@Lees-MBP.fc.ca>
2019-03-21 16:45:08 -0400

    Route template build finish Its gonna be my starting point

---
+++ config.js
@@ -0,0 +1,9 @@
+const config = {
+    user: "root",
+    password: "root", // leave this blank for windows
+    port: 8889, // for window 3306
+    host: "localhost",
+    database: "marvelcharacters"
+}
+
+module.exports = config;
